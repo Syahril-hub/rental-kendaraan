ac0a1f8a20afaf28aa3aee79854a6a77db4701d6
Syahril-hub <0001syahril@gmail.com>
2025-12-07 10:23:15 +0700

    Rename kendaraan table to kendaraans for consistency

--- database/migrations/2025_12_07_003023_create_kendaraan_table.php
+++ database/migrations/2025_12_07_003023_create_kendaraan_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('kendaraan', function (Blueprint $table) {
+        Schema::create('kendaraans', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
             $table->string('brand');
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('kendaraan');
+        Schema::dropIfExists('kendaraans');
     }
 };
