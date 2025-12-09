c95fc29d840d72fc692365db111c1e436e4dd085
Syahril-hub <0001syahril@gmail.com>
2025-12-09 08:19:10 +0700

    Add Pesanan model with user &kendaraan relations

---
+++ app/Models/Pesanan.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Kendaraan;
+
+class Pesanan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pesanans';
+
+    protected $fillable = [
+        'user_id',
+        'kendaraan_id',
+        'tanggal_mulai',
+        'tanggal_selesai',
+        'total_harga',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function kendaraan()
+    {
+        return $this->belongsTo(Kendaraan::class);
+    }
+}
